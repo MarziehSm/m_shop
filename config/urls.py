46fc11a1adc4701f707e6adffa5f325841805dfd
MarziehSm <lightestar98@gmail.com>
2023-04-03 04:22:50 +0430

    add mysqlclient library

--- config/urls.py
+++ config/urls.py
@@ -30,5 +30,6 @@ urlpatterns = [
 
     # rosetta
     path('rosetta/', include('rosetta.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
